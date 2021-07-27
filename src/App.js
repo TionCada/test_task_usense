d20f8704addcde8be9ceeb4d867dc66058dfc90c
Anton Novak <aantik802@gmail.com>
2021-07-27 16:58:04 +0300

    committing the whole project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
+import {interval} from 'rxjs';
+import s from './App.module.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const create_random_string = () => {
+        let random_string = '';
+        const string_length = 5;
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
+        for (let i = 0; i < string_length; i++) {
+            random_string += characters.charAt(Math.floor(Math.random() * characters.length))
+        }
+        return random_string
+    }
+
+    const checkPalindrom = (str) => {
+        return str === str.split('').reverse().join('');
+    }
+
+    const checkOnlyNumbers = (str) => {
+        return /^\d+$/.test(str);
+    }
+
+    const checkIfContainZero = (str) => {
+        return str.includes('0');
+    }
+
+    useEffect(() => {
+
+        const element = document.getElementById('result');
+        const source = interval(3000);
+        source.subscribe(val => {
+            let generated_string = create_random_string()
+            if (checkPalindrom(generated_string)) {
+                element.style.color = 'red'
+            }
+            if (checkOnlyNumbers(generated_string)) {
+                element.style.color = 'blue'
+            }
+            if (checkIfContainZero(generated_string)) {
+                element.style.display = 'none'
+            }
+            element.innerHTML = generated_string
+        });
+    })
+
+    return (
+        <div className={s.container}>
+            <div className={s.result}>
+                <p id="result"></p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
